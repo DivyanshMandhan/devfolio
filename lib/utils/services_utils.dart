1984e21ef43125d502d7616756e0c578caa72eb0
Divyanshmandhan-1 <divyanshmandhan3@gmail.com>
2022-07-03 16:30:46 +0530

    chnages

--- lib/utils/services_utils.dart
+++ lib/utils/services_utils.dart
@@ -15,7 +15,7 @@ class ServicesUtils {
 
   static const List<String> servicesDescription = [
     "Android app development via Flutter\n- Splash Screen\n- Firebase Auth/Cloud\n- REST APIs",
-    "Modern UI/UX Designing\n- Figma\n- Mobile & Web designs\n- Interactive UI designs\n- Responsiveness\n- Promo Videos and more..!",
+    "Modern UI/UX Designing\n- Figma\n- Mobile & Web designs\n- Interactive UI designs\n- Responsiveness",
     "Open source GitHub Projects\n- Awesome README.md\n- Well documented\n- Header images and more...!",
   ];
 }
