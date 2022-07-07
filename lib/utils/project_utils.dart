ff6c10d04ab0e0edc472dcee9b7271e218ea7696
Divyanshmandhan-1 <divyanshmandhan3@gmail.com>
2022-07-07 19:22:03 +0530

    changes

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -2,30 +2,35 @@ class ProjectUtils {
   static const List<String> banners = [
     "assets/projects/entera_verseW.png",
     "assets/projects/alan.png",
-    "assets/projects/mytodoW.png"
+    "assets/projects/mytodoW.png",
+    "assets/projects/newsappW.png",
   ];
 
   static const List<String> icons = [
     "assets/projects/entera_verse.png",
     "assets/projects/flutter.png",
     "assets/projects/mytodo.png",
+    "assets/projects/news.png",
   ];
 
   static const List<String> titles = [
     "Entera Verse",
     "Alan Radio",
-    "My Todo App"
+    "My Todo App",
+    "News App",
   ];
 
   static const List<String> description = [
     "An entertainment application and website made with flutter(Bloc and Cubits State Management is used).AWS S3 bucket to store data",
     "A Radio app with voice Assistant made with flutter(Velocity X is used for changing colors of pages when swiping).Alan Radio API.",
-    "A simpe todo application with Firebase as a backend."
+    "A simpe todo application with Firebase as a backend.",
+    "A news application with inshorts API.",
   ];
 
   static const List<String> links = [
     "https://github.com/DivyanshMandhan/Entera-Verse",
     "https://github.com/DivyanshMandhan/Alan---Radio",
     "https://github.com/DivyanshMandhan/my-todo",
+    "https://github.com/DivyanshMandhan/news",
   ];
 }
